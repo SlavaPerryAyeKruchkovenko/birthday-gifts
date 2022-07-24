7ee93a8b85d430cc1ef0426e9b5304e766eb1271
Slava <sh-e-95@mail.ru>
2022-07-24 20:55:19 +0500

    fix mistake

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ const replies = require("./replies")
 const server = new http.Server(
     micro(async (req, res) => {
         try{
-            const { request, session, state } = await micro.json(req);
-            const sessionState = state && state.session || {};
+            const {request, session} = await micro.json(req)
             const my_response = session.new
                 ? replies.welcome()
                 : {
-                    text: `hello ${session.user_id}`,
-                    tts: `<speaker audio="alice-music-harp-1.opus">hello ${session.user_id}`,
+                    text: `Выполняю ${request.command}`,
+                    tts: `<speaker audio="alice-music-harp-1.opus">Выполняю ${request.command}`,
                     buttons: [
                         { title: 'Записать подарок', hide: true },
                         { title: 'Посмотреть подарок', hide: true },
@@ -20,9 +19,8 @@ const server = new http.Server(
                 };
             return {
                 response: my_response,
-                session_state: sessionState,
                 version: '1.0'
-            };
+            }
         }
         catch (e){
             return{
@@ -31,9 +29,9 @@ const server = new http.Server(
                     end_session: false
                 },
                 version: '1.0'
-            };
+            }
         }
     })
-);
+)
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`))
