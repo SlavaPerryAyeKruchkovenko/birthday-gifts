f26ee70d67d072668abec3875047925dab3b7fbf
Slava <sh-e-95@mail.ru>
2022-07-24 20:47:59 +0500

    test

--- src/index.js
+++ src/index.js
@@ -4,25 +4,35 @@ const replies = require("./replies")
 
 const server = new http.Server(
     micro(async (req, res) => {
-        console.log(req)
-        const { request, session, state } = await micro.json(req);
-        const sessionState = state && state.session || {};
-        const my_response = session.new
-            ? replies.welcome()
-            : {
-                text: `hello ${session.user_id}`,
-                tts: `<speaker audio="alice-music-harp-1.opus">hello ${session.user_id}`,
-                buttons: [
-                    { title: 'Записать подарок', hide: true },
-                    { title: 'Посмотреть подарок', hide: true },
-                ],
-                end_session: false
+        try{
+            const { request, session, state } = await micro.json(req);
+            const sessionState = state && state.session || {};
+            const my_response = session.new
+                ? replies.welcome()
+                : {
+                    text: `hello ${session.user_id}`,
+                    tts: `<speaker audio="alice-music-harp-1.opus">hello ${session.user_id}`,
+                    buttons: [
+                        { title: 'Записать подарок', hide: true },
+                        { title: 'Посмотреть подарок', hide: true },
+                    ],
+                    end_session: false
+                };
+            return {
+                response: my_response,
+                session_state: sessionState,
+                version: '1.0'
             };
-        return {
-            response: my_response,
-            session_state: sessionState,
-            version: '1.0'
-        };
+        }
+        catch (e){
+            return{
+                response: {
+                    text: 'Произошла ошибка',
+                    end_session: false
+                },
+                version: '1.0'
+            };
+        }
     })
 );
 const PORT = process.env.PORT || 3000
